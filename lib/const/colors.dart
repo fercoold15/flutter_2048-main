dabee83799e19c77f921f0b624e201e13cea66e6
Fercoold <fernandomaldonado751@gmail.com>
2023-12-13 10:44:08 -0600

    Initial commit

---
+++ lib/const/colors.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+const backgroundColor = Color.fromRGBO(184, 242, 247, 0.68);
+const textColor = Color.fromARGB(255, 0, 0, 0);
+const titleColor = Color.fromARGB(255, 45, 10, 64);
+const textColorWhite = Color(0xfff9f6f2);
+const boardColor = Color.fromRGBO(62, 48, 169, 1);
+const emptyTileColor = Color.fromARGB(255, 73, 9, 88);
+const buttonColor = Color.fromARGB(255, 98, 45, 179);
+const scoreColor = Color.fromARGB(255, 45, 10, 64);
+const overlayColor = Color.fromRGBO(49, 45, 154, 0.722);
+const color2 = Color.fromARGB(255, 177, 178, 245);
+const color4 = Color.fromARGB(255, 173, 122, 251);
+const color8 = Color.fromARGB(255, 160, 100, 250);
+const color16 = Color.fromARGB(255, 140, 67, 249);
+const color32 = Color.fromARGB(255, 119, 34, 248);
+const color64 = Color.fromARGB(255, 102, 0, 254);
+const color128 = Color.fromARGB(255, 72, 2, 178);
+const color256 = Color.fromARGB(255, 6, 74, 111);
+const color512 = Color.fromARGB(255, 5, 217, 255);
+const color1024 = Color.fromARGB(255, 54, 182, 205);
+const color2048 = Color.fromARGB(255, 39, 204, 146);
+
+const tileColors = {
+  2: color2,
+  4: color4,
+  8: color8,
+  16: color16,
+  32: color32,
+  64: color64,
+  128: color128,
+  256: color256,
+  512: color512,
+  1024: color1024,
+  2048: color2048,
+};
